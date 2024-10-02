090a2e18d795d436dd7ef50cd193e99150d84743
jlw_4049 <jessielw4049@gmail.com>
2024-10-02 14:24:48 -0400

    feat: update docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,27 +6,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'deluge_web_client'
-copyright = '2024, jessielw'
-author = 'jessielw'
+project = "deluge_web_client"
+copyright = "2024, jessielw"
+author = "jessielw"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 
 import os
 import sys
-sys.path.insert(0,os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath(".."))
