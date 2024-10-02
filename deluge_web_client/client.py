0ca8ef31a11ee8e120a1fb6845922bfc7f7f7fb3
jlw_4049 <jessielw4049@gmail.com>
2024-10-02 09:41:29 -0400

    refactor: use typing Union instead of newer | operator, to expand compatibility for more python versions refactor: add a couple TODO's feat: docstring all methods feat: optimize the code substantially where each call utilizes a helper method to make the calls in a context manager

--- deluge_web_client/client.py
+++ deluge_web_client/client.py
@@ -2,13 +2,16 @@ import base64
 import requests
 from os import PathLike
 from pathlib import Path
+from typing import Union
 
 from .exceptions import DelugeWebClientError
+from .response import Response
 
 
 # TODO: timeout
-# TODO: check custom raised error message for each method
 # TODO: add other useful rpc methods https://deluge.readthedocs.io/en/deluge-2.0.1/reference/api.html
+# TODO: add method upload_torrents
+# TODO: setup this for having potential as a context manager completely that automatically calls close
 
 
 class DelugeWebClient:
@@ -20,39 +23,35 @@ class DelugeWebClient:
         self.url = self._build_url(url)
         self.password = password
 
-    def login(self):
+    def login(self) -> Response:
+        """Log in to Web UI"""
         payload = {"method": "auth.login", "params": [self.password], "id": 1}
-        response = self.session.post(self.url, headers=self.HEADERS, json=payload)
-        if response.ok:
-            result = response.json()
-            # print(result)
-            if result.get("result") == True:
-                return True
-            else:
-                raise DelugeWebClientError(f"Failed to login to client: {result}")
-        else:
-            response.raise_for_status()
+        return self.execute_call(payload)
 
-    def logout(self) -> tuple[bool, str]:
+    def logout(self) -> Response:
+        """Disconnects from the Web UI"""
         payload = {"method": "web.disconnect", "params": [], "id": 1}
-        response = self.session.post(self.url, headers=self.HEADERS, json=payload)
-        if response.ok:
-            result = response.json()
-            if result.get("result") == "Connection was closed cleanly.":
-                return True, "Connection was closed cleanly"
-            else:
-                return False, "Failed to logout of client"
-        else:
-            response.raise_for_status()
+        return self.execute_call(payload)
 
     def upload_torrent(
         self,
-        torrent_path: PathLike[str] | Path,
+        torrent_path: Union[PathLike[str], Path],
         save_directory: str = None,
         label: str = None,
-    ) -> tuple[bool, str]:
-        torrent_path = Path(torrent_path)
+    ) -> Response:
+        """
+        Opens the torrent path building out numerous payloads as needed to
+        upload a single torrent to the client.
 
+        Args:
+            torrent_path (PathLike[str], Path): Path to torrent file (example.torrent).
+            save_directory (str, optional): Defined path where the file should go on the host. Defaults to None.
+            label (str, optional): Label to apply to uploaded torrent. Defaults to None.
+
+        Returns:
+            Response: _description_
+        """
+        torrent_path = Path(torrent_path)
         with open(torrent_path, "rb") as tf:
             args = {"add_paused": True, "seed_mode": True, "auto_managed": True}
             if save_directory:
@@ -62,97 +61,145 @@ class DelugeWebClient:
                 str(base64.b64encode(tf.read()), encoding="utf-8"),
                 args,
             ]
-
             payload = {"method": "core.add_torrent_file", "params": params, "id": 1}
+            with self.session.post(
+                self.url, headers=self.HEADERS, json=payload
+            ) as response:
+                if response.ok:
+                    result = response.json()
+                    info_hash = str(result["result"])
+                    if label:
+                        self._apply_label(info_hash, str(label))
+                    self._start_torrent(info_hash)
+                    return Response(result=True, error=None, id=1)
+                else:
+                    response.raise_for_status()
 
-            response = self.session.post(self.url, headers=self.HEADERS, json=payload)
-            if response.ok:
-                result = response.json()
-                info_hash = str(result["result"])
-                if label:
-                    self._apply_label(info_hash, str(label))
-                self._start_torrent(info_hash)
-                return True, "Torrent injected and started"
-            else:
-                response.raise_for_status()
+    def _apply_label(self, info_hash: str, label: str) -> tuple[bool, bool]:
+        """
+        Used internally to add and apply labels as needed for
+        the `upload_torrent` method
 
-    def _apply_label(self, info_hash: str, label: str):
-        label = label.lower()
-        payload1 = {"method": "label.add", "params": [label], "id": 1}
-        create_label = self.session.post(self.url, headers=self.HEADERS, json=payload1)
-        if not create_label.ok or create_label.json()["error"] != None:
-            # TODO: throw an error!
-            pass
-        print(create_label.json())
+        Args:
+            info_hash (str): Info has of torrent.
+            label (str): Label to apply it to (automatically set to lowercase internally).
 
-        payload2 = {
+        Returns:
+            tuple (bool, bool): add_label(), set_label().
+        """
+        add_label = self.add_label(label)
+        set_label = self.set_label(info_hash, label)
+        return add_label, set_label
+
+    def _start_torrent(self, info_hash: str) -> Response:
+        """Helper method to start the torrent after it's been uploaded"""
+        payload = {"method": "core.resume_torrent", "params": [info_hash], "id": 1}
+        return self.execute_call(payload)
+
+    def get_labels(self) -> Response:
+        """Gets defined labels"""
+        payload = {"method": "label.get_labels", "params": [], "id": 1}
+        return self.execute_call(payload)
+
+    def set_label(self, info_hash: str, label: str) -> Response:
+        """Sets the label for a specific torrent"""
+        payload = {
             "method": "label.set_torrent",
-            "params": [info_hash, label],
+            "params": [info_hash, label.lower()],
             "id": 1,
         }
-        set_label = self.session.post(self.url, headers=self.HEADERS, json=payload2)
-        if not set_label.ok or set_label.json()["error"] != None:
-            # TODO: throw error
-            pass
-        print(set_label.json())
+        return self.execute_call(payload)
 
-    def _start_torrent(self, info_hash: str) -> None:
-        payload = {"method": "core.resume_torrent", "params": [info_hash], "id": 1}
-        response = self.session.post(self.url, headers=self.HEADERS, json=payload)
-        if not response.ok or response.json()["error"] != None:
-            # TODO: throw error
-            pass
+    def add_label(self, label: str) -> Response:
+        """Adds a label to the client, ignoring labels if they already exist"""
+        payload = {
+            "method": "label.add",
+            "params": [label.lower()],
+            "id": 1,
+        }
+        response = self.execute_call(payload, handle_error=False)
+        if response.error != None:
+            if "Label already exists" not in response.error.get("message"):
+                raise DelugeWebClientError(f"Error adding label:\n{response.error}")
+        return response
 
-    # TODO:
-    # def add_torrents(self, torrents: list[PathLike[str]]):
-
-    def check_connected(self) -> tuple[bool, str]:
+    def check_connected(self) -> Response:
+        """
+        Use the `web.connected` method to get a boolean response if the Web UI is
+        connected to a deluged host
+        """
         payload = {"method": "web.connected", "params": [], "id": 1}
-        response = self.session.post(self.url, headers=self.HEADERS, json=payload)
-        if response.ok:
-            result = response.json()
-            if result.get("result") == True:
-                return True, "Connected"
-            else:
-                return False, "Not connected"
-        else:
-            response.raise_for_status()
+        return self.execute_call(payload)
 
-    def get_hosts(self) -> list[list[str]]:
+    def get_hosts(self) -> Response:
+        """Returns hosts we're connected to currently"""
         payload = {"method": "web.get_hosts", "params": [], "id": 1}
-        response = self.session.post(self.url, headers=self.HEADERS, json=payload)
-        if response.ok:
-            result = response.json()
-            if result.get("result"):
-                return result.get("result")
-            else:
-                return []
-        else:
-            response.raise_for_status()
+        return self.execute_call(payload)
 
-    def get_host_status(self, host_id: str) -> list[str]:
+    def get_host_status(self, host_id: str) -> Response:
+        """Get the deluged host status `<hostID>`"""
         payload = {"method": "web.get_host_status", "params": [host_id], "id": 1}
-        response = self.session.post(self.url, headers=self.HEADERS, json=payload)
-        if response.ok:
-            result = response.json()
-            if result.get("result"):
-                return result.get("result")
-            else:
-                return []
-        else:
-            response.raise_for_status()
+        return self.execute_call(payload)
 
-    def connect_to_host(self, host_id: str) -> list[str]:
+    def connect_to_host(self, host_id: str) -> Response:
+        """To connect to deluged with `<hostID>`"""
         payload = {"method": "web.connect", "params": [host_id], "id": 1}
-        response = self.session.post(self.url, headers=self.HEADERS, json=payload)
-        if response.ok:
-            result = response.json()
-            if result.get("result"):
-                return result.get("result")
+        return self.execute_call(payload)
+
+    def test_listen_port(self) -> bool:
+        """Checks if the active port is open
+
+        Returns:
+            bool: If active port is opened or closed
+        """
+        payload = {"method": "core.test_listen_port", "params": [], "id": 1}
+        return self.execute_call(payload).result
+
+    def execute_call(self, payload: dict, handle_error: bool = True) -> Response:
+        # TODO: finish this
+        """
+        Helper method to execute most calls to the Web UI as needed.
+
+        Args:
+            payload (dict): Payload object to be called.
+            handle_error (bool, optional): Handle errors here or allow the caller to handle
+                the error. Defaults to True.
+
+        Returns:
+            Response: Response object for each call.
+        """
+        with self.session.post(
+            self.url, headers=self.HEADERS, json=payload
+        ) as response:
+            if response.ok:
+                response_json = response.json()
+                print(response_json)
+                data = Response(
+                    result=response_json.get("result"),
+                    error=self._normalize_exception(response_json.get("error")),
+                    id=response_json.get("id"),
+                )
+                if handle_error and data.error:
+                    raise DelugeWebClientError(
+                        f"Payload: {payload}, Error: {data.error}"
+                    )
+                return data
             else:
-                return []
-        else:
-            response.raise_for_status()
+                raise DelugeWebClientError(
+                    f"Failed to execute call. Response code: {response.status_code}. Reason: {response.reason}"
+                )
+
+    @staticmethod
+    def _normalize_exception(exc_str: any) -> Union[str, None]:
+        """
+        Removes the un-needed ending square bracket and stripping extra white
+        space if input is a string
+        """
+        if exc_str:
+            if isinstance(exc_str, str):
+                return exc_str.rstrip("]").strip()
+            else:
+                return exc_str
 
     @staticmethod
     def _build_url(url: str) -> str:
