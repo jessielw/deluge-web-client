fb6ebc6698085503f55c2f84b3bc52c053f2dfef
jlw_4049 <jessielw4049@gmail.com>
2024-10-05 14:10:50 -0400

    refactor: add `__all__`

--- deluge_web_client/__init__.py
+++ deluge_web_client/__init__.py
@@ -1,3 +1,5 @@
 from .client import DelugeWebClient
 from .response import Response
 from .exceptions import DelugeWebClientError
+
+__all__ = ["DelugeWebClient", "Response", "DelugeWebClientError"]
